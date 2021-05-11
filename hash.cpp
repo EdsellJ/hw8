9f4f7cac2cb63590b72b1a3594e9b32f0efada07
Jacob Edsell <jedsell72@gmail.com>
2021-05-11 16:29:04 -0700

    Finished

--- hash.cpp
+++ hash.cpp
@@ -1,17 +1,19 @@
 #include "hash.h"
-
+//constructor
 HashMap::HashMap(int size) {
     this->size = size;
     table = new Entry*[size];
     for (int i= 0; i < size; i++)
         table[i] = NULL;
 }
+//Deconstructor
 HashMap::~HashMap() {
     for (int i = 0; i < size; i++)                  
 		if (table[i] != NULL)                        
 			delete table[i];            
 	delete[] table;
 }
+//add entries to hashmap
 void HashMap::put(Entry *e){
     string key = e->getKey();
     int index = myHash(key) % size;
@@ -22,7 +24,7 @@ void HashMap::put(Entry *e){
     }
     table[index] = e;
 }
-
+//get data from the hashmap
  Entry* HashMap::get(string key){
     int index = myHash(key) % size;
     int startIndex = index;
@@ -32,7 +34,13 @@ void HashMap::put(Entry *e){
     }
     return table[index];
 }
+//Hashing a using DJB2 hash algorithm
 unsigned long HashMap::myHash(string str){
-
+    const char* cstr = str.c_str();
+    unsigned long hash = 5381;
+    int c;
+    while (c = *cstr++)
+        hash = hash * 33 + c;
+    return hash;
 }
 
